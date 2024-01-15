ff8d74ad7ad1c2e3a413dbf2e0c3d35383fe25bd
Brent V. Bingham <bvbgrad@gmail.com>
2024-01-15 04:39:00 -0600

    Add form buttons. Output posts as a list.

--- ref01/src/components/NewPost.jsx
+++ ref01/src/components/NewPost.jsx
@@ -1,23 +1,46 @@
-// import { useState } from 'react'
+import { useState } from 'react';
 
 import classes from './NewPost.module.css';
 
-function NewPost(props) {
-  // const[enteredBody, setEnteredBody] = useState('')
-  
-  // function changeBodyHandler(event) {
-  //   setEnteredBody(event.target.value)
-  // }
-  
+function NewPost({ onCancel, onAddPost }) {
+  const [enteredBody, setEnteredBody] = useState('');
+  const [enteredAuthor, setEnteredAuthor] = useState('');
+
+  function bodyChangeHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+
+  function authorChangeHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
+
+  function submitHandler(event) {
+    event.preventDefault();
+    setEnteredAuthor(event.target.value);
+    const postData = {
+      body: enteredBody,
+      author: enteredAuthor,
+    };
+    console.log(postData);
+    onAddPost(postData);
+    onCancel();
+  }
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <p>
-        <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required onChange={props.onAuthorChange}/>
+        <label htmlFor='name'>Your name</label>
+        <input type='text' id='name' required onChange={authorChangeHandler} />
       </p>
       <p>
-        <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} onChange={props.onBodyChange}/>
+        <label htmlFor='body'>Text</label>
+        <textarea id='body' required rows={3} onChange={bodyChangeHandler} />
+      </p>
+      <p className={classes.actions}>
+        <button type='button' onClick={onCancel}>
+          Cancel
+        </button>
+        <button>Submit</button>
       </p>
     </form>
   );
--- ref01/src/components/PostsList.jsx
+++ ref01/src/components/PostsList.jsx
@@ -1,37 +1,37 @@
-import { useState } from 'react';
-
 import Post from './Post';
 import NewPost from './NewPost';
 import classes from './PostsList.module.css';
 import Modal from './Modal';
+import { useState } from 'react';
 
-function PostsList({isPosting, onStopPosting}) {
-  const [enteredBody, setEnteredBody] = useState('');
-  const [enteredAuthor, setEnteredAuthor] = useState('');
+function PostsList({ isPosting, onStopPosting }) {
+  const [posts, setPosts] = useState([]);
 
-  function bodyChangeHandler(event) {
-    setEnteredBody(event.target.value);
-  }
-
-  function authorChangeHandler(event) {
-    setEnteredAuthor(event.target.value);
+  function addPostHandler(postData) {
+    // best practice for updating state if update depends on prior state
+    setPosts((existingPosts) => [postData, ...existingPosts]);
   }
 
   return (
     <>
       {isPosting && (
         <Modal onClose={onStopPosting}>
-          <NewPost
-            onClick={onStopPosting}
-            onBodyChange={bodyChangeHandler}
-            onAuthorChange={authorChangeHandler}
-          />
+          <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
         </Modal>
       )}
-      <ul className={classes.posts}>
-        <Post author={enteredAuthor} body={enteredBody} />
-        <Post author='Max' body='body2' />
-      </ul>
+      {posts.length > 0 && (
+        <ul className={classes.posts}>
+          {posts.map((post) => (
+            <Post key={post.body} author={post.author} body={post.body} />
+          ))}
+        </ul>
+      )}
+      {posts.length === 0 && (
+        <div style={{ textAlign: 'center', color: 'white' }}>
+          <h2>There are no posts yet.</h2>
+          <p>Start adding some posts.</p>
+        </div>
+      )}
     </>
   );
 }
