45685b35eeeb1f4e6851db4f9976398537cd3592
Brent V. Bingham <bvbgrad@gmail.com>
2024-01-29 05:05:26 -0600

    Output meals data & images with unknown dimensions

--- foodies/app/meals/page.jsx
+++ foodies/app/meals/page.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import Link from 'next/link';
+import classes from './page.module.css';
+import MealsGrid from '@/components/meals/meals-grid';
 
-function MealsPage() {
+export default function MealsPage() {
   return (
-    <div><h1>MealsPage</h1></div>
-  )
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{' '}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>Choose your favorite recipe</p>
+        <p className={classes.cta}>
+          <Link href='/meals/share'>Share your favorite recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
+  );
 }
-
-export default MealsPage
